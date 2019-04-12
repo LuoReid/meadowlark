e881302baa08e1d28ba0a1eedbc651b0d51983ed
Luo Reid <luo.reid@gmail.com>
2019-04-13 00:32:33 +0800

    handlebars demo

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -7,23 +7,19 @@ app.set('view engine','handlebars')
 
 app.set('port', process.env.PORT || 3000)
 app.get('/',function(req,res){
-  res.type('text/plain')
-  res.send('Meadowlark Travel')
+  res.render('home')
 })
 app.get('/about',function(req,res){
-  res.type('text/plain')
-  res.send('About Meadowlark Travel')
+  res.render('about')
 })
 app.use(function (req, res) {
-  res.type('text/plain')
   res.status(404)
-  res.send('404 - Not Found')
+  res.render('404')
 })
 app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.type('text/plain')
+  console.error(err.stack)
   res.status(500)
-  res.send('500 - Server Error')
+  res.render('500')
 })
 app.listen(app.get('port'), function () {
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.')
