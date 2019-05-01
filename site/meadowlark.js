c75792cb5f585e0210c89585a06a593a6e0322a9
Luo Reid <luo.reid@gmail.com>
2019-05-01 19:19:40 +0800

    cluster

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -350,10 +350,26 @@ app.use(function(err, req, res, next) {
   res.render('500');
 });
 
-app.listen(app.get('port'), function() {
-  console.log(
-    `Express started in ${app.get('env')} mode on http://localhost:${app.get(
-      'port'
-    )}; press Ctrl-C to terminate.`
-  );
-});
+// app.listen(app.get('port'), function() {
+//   console.log(
+//     `Express started in ${app.get('env')} mode on http://localhost:${app.get(
+//       'port'
+//     )}; press Ctrl-C to terminate.`
+//   );
+// });
+
+var http = require('http');
+function startServer() {
+  http.createServer(app).listen(app.get('port'), function() {
+    console.log(
+      `Express started in ${app.get('env')} mode on http://localhost:${app.get(
+        'port'
+      )};press Ctrl-C to terminate.`
+    );
+  });
+}
+if (require.main === module) {
+  startServer();
+} else {
+  module.exports = startServer;
+}
