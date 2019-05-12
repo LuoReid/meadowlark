b082cabb54eb3314b472a66a70e5069a4dd07348
Luo Reid <luo.reid@gmail.com>
2019-05-12 23:17:20 +0800

    provide api

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -554,6 +554,49 @@ app.use(function(req,res,next){
   next();
 })
 
+var Attraction = require('./models/attraction.js');
+app.get('/api/attractions',function(req,res){
+  Attraction.find({approved:true},function(err,attractions){
+    if(err)return res.send(500,'Error occurred: database error.');
+    res.json(attractions.map(function(a){
+      return {
+        name:a.name,
+        id:a._id,
+        description:a.description,
+        location:a.location,
+      }
+    }));
+  });
+});
+app.post('/api/attraction',function(req,res){
+  var a = new Attraction({
+    name:req.body.name,
+    description:req.body.description,
+    location:{lat:req.body.lat,lng:req.body.lng},
+    history:{
+      event:'created',
+      email:req.body.email,
+      date:new Date(),
+    },
+    approved:false,
+  });
+  a.save(function(err,a){
+    if(err)return res.send(500,'Error occurred: database error.');
+    res.json({id:a._id});
+  });
+});
+app.get('/api/attraction/:id',function(req,res){
+  Attraction.findById(req.params.id,function(err,a){
+    if(err)return res.send(500,'Error occurred: database error.');
+    res.json({
+      name:a.name,
+      id:a._id,
+      description:a.description,
+      location:a.location,
+    });
+  });
+});
+
 app.use(function (req, res) {
   res.status(404);
   res.render('404');
