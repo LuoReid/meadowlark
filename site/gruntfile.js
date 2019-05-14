53db35033b4310128308b55dce0f457a40ce68dc
Luo Reid <luo.reid@gmail.com>
2019-05-14 23:52:39 +0800

    css static resource

--- site/gruntfile.js
+++ site/gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function (grunt) {
-  ['grunt-cafe-mocha', 'grunt-contrib-jshint', 'grunt-exec'].forEach(function (task) {
+  ['grunt-cafe-mocha', 'grunt-contrib-jshint', 'grunt-exec', 'grunt-contrib-less'].forEach(function (task) {
     grunt.loadNpmTasks(task);
   });
   grunt.initConfig({
@@ -8,7 +8,21 @@ module.exports = function (grunt) {
       app: ['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js'],
       qa: ['gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js']
     },
-    exec: { linkchecker: { cmd: 'linkchecker http://localhost:3000' } }
+    exec: { linkchecker: { cmd: 'linkchecker http://localhost:3000' } },
+    less: {
+      development: {
+        options:{
+          customFunctions:{
+            static:function(lessObject,name){
+              return `url("${require('./lib/static.js').map(name.value)}")`;
+            }
+          }
+        },
+        files: {
+          'public/css/main.css': 'less/main.less',
+        }
+      }
+    }
   });
-  grunt.registerTask('default', ['cafemocha', 'jshint', 'exec']);
+  grunt.registerTask('default', ['cafemocha', 'jshint', 'exec','less']);
 };
