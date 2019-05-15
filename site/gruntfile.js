c0a62af54e9cf3944a7af01b9dbbf645c1e1bcba
Luo Reid <luo.reid@gmail.com>
2019-05-16 01:05:24 +0800

    combine and minify

--- site/gruntfile.js
+++ site/gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function (grunt) {
-  ['grunt-cafe-mocha', 'grunt-contrib-jshint', 'grunt-exec', 'grunt-contrib-less'].forEach(function (task) {
-    grunt.loadNpmTasks(task);
-  });
+  ['grunt-cafe-mocha', 'grunt-contrib-jshint', 'grunt-exec', 'grunt-contrib-less',
+
+    'grunt-contrib-uglify', 'grunt-contrib-cssmin', 'grunt-hashres'].forEach(function (task) {
+      grunt.loadNpmTasks(task);
+    });
   grunt.initConfig({
     cafemocha: { all: { src: 'qa/test-*.js', options: { id: 'tdd' }, }, },
     jshint: {
@@ -11,18 +13,43 @@ module.exports = function (grunt) {
     exec: { linkchecker: { cmd: 'linkchecker http://localhost:3000' } },
     less: {
       development: {
-        options:{
-          customFunctions:{
-            static:function(lessObject,name){
+        options: {
+          customFunctions: {
+            static: function (lessObject, name) {
               return `url("${require('./lib/static.js').map(name.value)}")`;
             }
           }
         },
         files: {
           'public/css/main.css': 'less/main.less',
+          'public/css/cart.css': 'less/cart.css'
         }
+      }
+    },
+    uglify: {
+      all: { files: { 'public/js/meadowlark.min.js': ['public/js/**/*.js'] } }
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'public/css/meadowlark.css': ['public/css/**/*.css', '!public/css/meadowlark*.css']
+        }
+      },
+      minify: {
+        src: 'public/css/meadowlark.css',
+        dest: 'public/css/meadowlark.min.css',
+      }
+    },
+    hashres: {
+      options: {
+        fileNameFormat: `${name}.${hash}.${ext}`
+      },
+      all: {
+        src: ['public/js/meadowlark.min.js', 'public/css/meadowlark.min.js'],
+        dest: ['views/layouts/main.handlebars'],
       }
     }
   });
-  grunt.registerTask('default', ['cafemocha', 'jshint', 'exec','less']);
+  grunt.registerTask('default', ['cafemocha', 'jshint', 'exec']);
+  grunt.registerTask('static',['less','cssmin','uglify','hashres']);
 };
