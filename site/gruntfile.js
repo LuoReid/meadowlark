0cb62d816151360344a86d34e64d9ea7ed63ec61
Luo Reid <luo.reid@gmail.com>
2019-05-20 23:49:48 +0800

    qa static

--- site/gruntfile.js
+++ site/gruntfile.js
@@ -1,7 +1,12 @@
 module.exports = function (grunt) {
-  ['grunt-cafe-mocha', 'grunt-contrib-jshint', 'grunt-exec', 'grunt-contrib-less',
-
-    'grunt-contrib-uglify', 'grunt-contrib-cssmin', 'grunt-hashres'].forEach(function (task) {
+  ['grunt-cafe-mocha',
+    'grunt-contrib-jshint',
+    'grunt-exec',
+    'grunt-contrib-less',
+    'grunt-contrib-uglify',
+    'grunt-contrib-cssmin',
+    'grunt-hashres',
+    'grunt-lint-pattern'].forEach(function (task) {
       grunt.loadNpmTasks(task);
     });
   grunt.initConfig({
@@ -48,8 +53,35 @@ module.exports = function (grunt) {
         src: ['public/js/meadowlark.min.js', 'public/css/meadowlark.min.js'],
         dest: ['config.js'],
       }
+    },
+    lint_pattern: {
+      view_statics: {
+        options: {
+          rules: [
+            {
+              pattern: /<link [^>]*href=["'](?!\{\{static)/,
+              message: 'Un-mapped static resource found in <link>.'
+            },
+            {
+              pattern: /<script [^>]*src=["'](?!\{\{static)/,
+              message: 'Un-mapped static resource found in <script>.'
+            },
+            {
+              pattern: /<img [^>]*src=["'](?!\{\{static)/,
+              message: 'Un-mapped static resource found in <img>.'
+            }
+          ]
+        },
+        files: { src: ['views/**/*.handlebars'] }
+      }
+    },
+    css_statics: {
+      options: {
+        rules: [{ pattern: /url\(/, message: 'Un-mapped static found in LESS property.' }]
+      },
+      files:{src:['less/**/*.less']}
     }
   });
-  grunt.registerTask('default', ['cafemocha', 'jshint', 'exec']);
-  grunt.registerTask('static',['less','cssmin','uglify','hashres']);
+  grunt.registerTask('default', ['cafemocha', 'jshint', 'exec','lint_pattern']);
+  grunt.registerTask('static', ['less', 'cssmin', 'uglify', 'hashres']);
 };
